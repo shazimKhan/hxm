293bc36958658840ab803865e74b2331d31fb9d4
shazimkhan <shazim96khan@gmail.com>
2021-05-02 15:34:58 +0500

    shazim changes

--- client/components/LeaveManagement.vue
+++ client/components/LeaveManagement.vue
@@ -1,83 +1,6 @@
 <template>
   <div>
-    <a-row>
-      <a-col :span="24" class="">
-        <a-page-header
-          style="border: 1px solid rgb(235, 237, 240)"
-          :title="false"
-        >
-          <a-row>
-            <a-col :span="1" class="pt-1">
-              <span class="font-weight-bold">Search</span>
-            </a-col>
-            <a-col :span="3">
-              <a-select
-                default-value="work"
-                style="width: 100%"
-                @change="handleChange"
-              >
-                <a-icon slot="suffixIcon" type="search" />
-                <a-select-option value="work">
-                  Work Schedule
-                </a-select-option>
-              </a-select>
-            </a-col>
-            <a-col :span="4" class="pl-4">
-              <a-select
-                default-value="8hr7to15"
-                style="width: 100%"
-                @change="handleChange"
-              >
-                <a-icon slot="suffixIcon" type="search" />
-                <a-select-option value="8hr7to15">
-                  8hr7to15 (8hr7to15)
-                </a-select-option>
-              </a-select>
-            </a-col>
-            <a-col :span="3" class="pt-1 pl-4">
-              <span class="font-weight-bold">Includes Inactive</span>
-            </a-col>
-            <a-col :span="2">
-              <a-select
-                default-value="no"
-                style="width: 100%"
-                @change="handleChange"
-              >
-                <a-icon slot="suffixIcon" type="search" />
-                <a-select-option value="no">
-                  No
-                </a-select-option>
-              </a-select>
-            </a-col>
-            <a-col :span="11" align="end" class="pt-1">
-              <a-row>
-                <a-col :span="16" class="pt-1 pr-3">
-                  <span class="font-weight-bold">Create New</span>
-                </a-col>
-                <a-col :span="8">
-                  <a-select
-                    default-value="no"
-                    style="width: 100%"
-                    @change="handleChange"
-                  >
-                    <a-icon slot="suffixIcon" type="search" />
-                    <a-select-option value="no">
-                      No Selection
-                    </a-select-option>
-                  </a-select>
-                </a-col>
-              </a-row>
-            </a-col>
-          </a-row>
-
-          <!-- <template slot="extra">
-            <a-button key="3"> Operation </a-button>
-            <a-button key="2"> Operation </a-button>
-            <a-button key="1" type="primary"> Primary </a-button>
-          </template> -->
-        </a-page-header>
-      </a-col>
-    </a-row>
+ 
     <a-row>
       <a-card :title="'  Holiday Caldendar: Holcal (Holcal)'">
         <a-row>
@@ -102,7 +25,7 @@
             </a-row>
             <a-row :gutter="16" class="my-2">
               <a-col :span="4" class="text-right">
-                <p> Country</p>
+                <p>Country</p>
               </a-col>
               <a-col :span="4">
                 <a-select
@@ -111,9 +34,7 @@
                   @change="handleChange"
                 >
                   <a-icon slot="suffixIcon" type="search" />
-                  <a-select-option value="work">
-                    No Selection
-                  </a-select-option>
+                  <a-select-option value="work"> No Selection </a-select-option>
                 </a-select>
               </a-col>
               <a-col :span="1">
@@ -144,12 +65,21 @@
         </a-row>
         <a-row>
           <a-col :span="24">
-            <a-table :columns="columns" :data-source="data" :bordered="false" :stripped="false">
+            <a-table
+              :columns="columns"
+              :data-source="data"
+              :pagination="false"
+              :bordered="false"
+              :stripped="false"
+            >
               <template slot="title">
                 <h6>Holiday Assignments</h6>
               </template>
               <span slot="date">
-                <a-date-picker :default-value="moment('01/01/2018', dateFormat)" :format="dateFormat" />
+                <a-date-picker
+                  :default-value="moment('01/01/2018', dateFormat)"
+                  :format="dateFormat"
+                />
               </span>
               <span slot="full">
                 <a-select
@@ -158,9 +88,7 @@
                   @change="handleChange"
                 >
                   <a-icon slot="suffixIcon" type="search" />
-                  <a-select-option value="work">
-                    Full
-                  </a-select-option>
+                  <a-select-option value="work"> Full </a-select-option>
                 </a-select>
               </span>
               <span slot="holiday">
@@ -196,13 +124,9 @@
               </span>
               <template slot="footer" class="bg-footer">
                 <div class="save-btn text-right">
-                  <a-button type="success">
-                    cancel
-                  </a-button>
+                  <a-button type="success"> cancel </a-button>
 
-                  <a-button type="primary">
-                    Save
-                  </a-button>
+                  <a-button type="primary"> Save </a-button>
                 </div>
               </template>
             </a-table>
@@ -214,68 +138,63 @@
   </div>
 </template>
 <script>
-import moment from 'moment'
+import moment from "moment";
 const columns = [
   {
-    title: 'Date Of Holiday',
-    dataIndex: 'date_of_holiday',
-    key: 'date_of_holiday',
-    scopedSlots: { customRender: 'date' },
-    width: 180
-
+    title: "Date Of Holiday",
+    dataIndex: "date_of_holiday",
+    key: "date_of_holiday",
+    scopedSlots: { customRender: "date" },
+    width: 180,
   },
   {
-    title: 'Holiday Class',
-    dataIndex: 'holiday_class',
-    scopedSlots: { customRender: 'full' },
-    key: 'holiday_class',
-    width: 120
-
+    title: "Holiday Class",
+    dataIndex: "holiday_class",
+    scopedSlots: { customRender: "full" },
+    key: "holiday_class",
+    width: 120,
   },
   {
-    title: 'Holiday',
-    dataIndex: 'holiday',
-    key: 'holiday',
-    scopedSlots: { customRender: 'holiday' },
-    width: 250
-
+    title: "Holiday",
+    dataIndex: "holiday",
+    key: "holiday",
+    scopedSlots: { customRender: "holiday" },
+    width: 250,
   },
   {
-    title: '',
-    dataIndex: 'planedHours',
-    key: 'planedHours',
-    scopedSlots: { customRender: 'icon' },
-    width: 120
+    title: "",
+    dataIndex: "planedHours",
+    key: "planedHours",
+    scopedSlots: { customRender: "icon" },
+    width: 120,
+  },
+];
 
-  }
-]
-
-const data = []
+const data = [];
 for (let i = 0; i < 5; i++) {
   data.push({
-    key: i
-  })
+    key: i,
+  });
 }
 export default {
-  data () {
+  data() {
     return {
-      dateFormat: 'DD/MM/YYYY',
-      monthFormat: 'MM/YYYY',
-      dateFormatList: ['DD/MM/YYYY', 'DD/MM/YY'],
+      dateFormat: "DD/MM/YYYY",
+      monthFormat: "MM/YYYY",
+      dateFormatList: ["DD/MM/YYYY", "DD/MM/YY"],
       lists: [1, 2, 3, 4],
       items: [1, 2, 3, 4],
       data,
-      columns
-
-    }
+      columns,
+    };
   },
   methods: {
     moment,
-    handleChange (val) {
-      console.log(val)
-    }
-  }
-}
+    handleChange(val) {
+      console.log(val);
+    },
+  },
+};
 </script>
 <style lang="scss">
 .bg-footer {
@@ -291,20 +210,17 @@ tr:last-child td {
     font-size: 20px;
   }
   i.anticon.anticon-question-circle {
-        font-size: 20px;
-            color: #eee;
+    font-size: 20px;
+    color: #eee;
   }
   i.anticon.anticon-plus-circle {
     font-size: 20px;
   }
   i.anticon.anticon-delete {
-        font-size: 20px;
-
+    font-size: 20px;
   }
 }
-  i.anticon.anticon-project {
-            font-size: 20px;
-
-  }
-
+i.anticon.anticon-project {
+  font-size: 20px;
+}
 </style>
--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <nav class="navbar navbar-expand-lg navbar-light bg-info">
     <div class="container">
       <router-link
@@ -103,6 +104,90 @@
       </div>
     </div>
   </nav>
+     <a-row>
+      <a-col :span="24" class="">
+        <a-page-header
+          style="border: 1px solid rgb(235, 237, 240)"
+          :title="false"
+        >
+          <a-row>
+            <a-col :span="1" class="pt-1">
+              <span class="font-weight-bold">Search</span>
+            </a-col>
+            <a-col :span="3">
+             <a-select
+              :default-value="moduleList"
+              style="width: 200px"
+              class="bg-info"
+              @change="getModuleValue"
+            >
+              <a-select-option value="admin-center"
+                >Admin Center</a-select-option
+              >
+              <a-select-option value="employee-profile"
+                >Employee profile</a-select-option
+              >
+              <a-select-option value="work-schedule"
+                >Work Schedule</a-select-option
+              >
+              <a-select-option value="leave-management"
+                >Leave Management</a-select-option
+              >
+            </a-select>
+            </a-col>
+            <a-col :span="4" class="pl-4">
+              <a-select
+                default-value="8hr7to15"
+                style="width: 100%"
+                @change="handleChange"
+              >
+                <a-icon slot="suffixIcon" type="search" />
+                <a-select-option value="8hr7to15">
+                  8hr7to15 (8hr7to15)
+                </a-select-option>
+              </a-select>
+            </a-col>
+            <a-col :span="3" class="pt-1 pl-4">
+              <span class="font-weight-bold">Includes Inactive</span>
+            </a-col>
+            <a-col :span="2">
+              <a-select
+                default-value="no"
+                style="width: 100%"
+                @change="handleChange"
+              >
+                <a-icon slot="suffixIcon" type="search" />
+                <a-select-option value="no"> No </a-select-option>
+              </a-select>
+            </a-col>
+            <a-col :span="11" align="end" class="pt-1">
+              <a-row>
+                <a-col :span="16" class="pt-1 pr-3">
+                  <span class="font-weight-bold">Create New</span>
+                </a-col>
+                <a-col :span="8">
+                  <a-select
+                    default-value="no"
+                    style="width: 100%"
+                    @change="handleChange"
+                  >
+                    <a-icon slot="suffixIcon" type="search" />
+                    <a-select-option value="no"> No Selection </a-select-option>
+                  </a-select>
+                </a-col>
+              </a-row>
+            </a-col>
+          </a-row>
+
+          <!-- <template slot="extra">
+            <a-button key="3"> Operation </a-button>
+            <a-button key="2"> Operation </a-button>
+            <a-button key="1" type="primary"> Primary </a-button>
+          </template> -->
+        </a-page-header>
+      </a-col>
+    </a-row>
+    </div>
 </template>
 
 <script>
