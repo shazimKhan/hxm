84252875eb4d2626798406129d847e1dd276beac
kanwalbatool072 <kanwalbatool014@gmail.com>
2021-05-05 21:32:13 +0500

    time-sheet

---
+++ client/components/TimeSheet/TimeSheet.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="time-sheet" />
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/pages/time-sheet/time-sheet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="time-sheet">
+    <TimeSheet />
+  </div>
+</template>
+
+<script>
+import TimeSheet from '@/components/TimeSheet/TimeSheet'
+export default {
+  components: {
+    TimeSheet
+  }
+}
+</script>
+
+<style>
+
+</style>
